8d0619deaa282b27741bdc59138015a5d9254392
rivalrexy <rivaldwirexy@gmail.com>
2021-12-21 20:41:47 +0700

    four commit

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://www.emsifa.com/api-wilayah-indonesia/api/",
+  baseURL: "https://www.emsifa.com/api-wilayah-indonesia/api/",
   headers: {
     "Content-type": "application/json",
   },
